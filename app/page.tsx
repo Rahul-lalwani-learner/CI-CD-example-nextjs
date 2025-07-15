ef90b4412162b6e8bc4931606c935e6c3c9c7e6f
Rahul <rahulkrishlalwani@gmail.com>
2025-07-15 17:23:34 +0530

    17:23

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-pink-300 via-pink-200 to-pink-300 text-center font-sans">
       <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold text-pink-600">🎉 Happy Birthday! 🎂</h1>
+        <h1 className="text-4xl font-bold text-pink-600">🎉 Happy Birthday Harkirat! 🎂</h1>
         <p className="text-lg text-pink-800 mt-4">From Rahul lalwani
         </p>
         <div className="text-9xl mt-5">🎂</div>
