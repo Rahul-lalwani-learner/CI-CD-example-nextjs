ba7a1cdecef1fe65dba1b45c917f716cbd9c37c3
Rahul <rahulkrishlalwani@gmail.com>
2025-07-15 16:27:56 +0530

    wishes from rahul lalwani

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gradient-to-r from-pink-300 via-pink-200 to-pink-300 text-center font-sans">
       <div className="bg-white p-8 rounded-lg shadow-lg">
         <h1 className="text-4xl font-bold text-pink-600">🎉 Happy Birthday! 🎂</h1>
-        <p className="text-lg text-pink-800 mt-4">Wishing you a day filled with love, joy, and laughter!</p>
+        <p className="text-lg text-pink-800 mt-4">Wishing you a day filled with love, joy, and laughter!
+        From Rahul Lalwani
+        </p>
         <div className="text-9xl mt-5">🎂</div>
       </div>
     </div>
